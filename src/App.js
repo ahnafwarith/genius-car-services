2dd875cb8ce7be43a6093cf899f8e3c1025563df
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-11 21:42:31 +0600

    created fakeData

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
-        <Route path='/' element></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='about' element={<About />}></Route>
       </Routes>
     </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Services = () => {
+    /* const services = [
+        { id: 1, name: 'repairer', address: 'mohammadpur', img: 'https://i.ibb.co/ZMMFMnH/repair-man.png' }
+    ] */
     return (
         <div>
             <h1>This is services</h1>
